837ba85381b9b6e873da672c1e2985eaf25f53aa
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-01-22 22:19:08 +0530

    added debounce

--- movie_app/script.js
+++ movie_app/script.js
@@ -17,8 +17,12 @@ const input = document.querySelector("input");
 // };
 
 //Here on this line of code what we do is make sure that the function fetchResult is called only after a second user stops typing
+// The whole process is called debouncing an input which is waiting for some time to pass after the last event to actually do something
 let timeoutId;
 
+/* What is happening on the code below is that we take setTimeout for the fetchResult and then after that it returns an Id and we store it
+and after doing that we take that timeout and clearTimeout and then we do this until the user doesn't type for one second
+*/
 const onInput = (e) => {
 	if (timeoutId) {
 		clearTimeout(timeoutId);
