e4e1e164371d0a6f3ac777f50828c96551b44a1f
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-08 21:46:34 +0530

    refactoring for the game

--- physics-game/script.js
+++ physics-game/script.js
@@ -1,18 +1,19 @@
-const { Engine, Render, Runner, World, Bodies, Mouse, MouseConstraint } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const width = 600
+const height = 600
 
 const engine = Engine.create();
 
 const { world } = engine;
 
-const width = 800
-const height = 600
-
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes: true,
+        width,
+        height
     }
 })
 
@@ -20,38 +21,13 @@ Render.run(render);
 
 Runner.run(Runner.create(), engine)
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}))
-
 //Walls 
 
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, {
-        isStatic: true
-    }),
-    Bodies.rectangle(400, 600, 800, 40, {
-        isStatic: true
-    }),
-    Bodies.rectangle(0, 300, 40, 600, {
-        isStatic: true
-    }),
-    Bodies.rectangle(800, 300, 40, 600, {
-        isStatic: true
-    })
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ]
 
-
 World.add(world, walls)
-
-
-// Random shapes
-
-for (let i = 0; i < 30; i++) {
-    if (Math.random() > 0.5) {
-        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50))
-    }
-    else {
-        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 30))
-    }
-}
