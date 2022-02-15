f850d33a03d082bcf31c4043fcdccbdd3b3af539
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-15 21:54:35 +0530

    started doing secret msg in JS

---
+++ secret_msg/script.js
@@ -0,0 +1,13 @@
+document.querySelector("form").addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const message = document.querySelector("#message-input")
+
+    const encrypted = btoa(message.value);
+
+    const linkInput = document.querySelector("#link-input")
+
+    linkInput.value = `${window.location}#${encrypted}`
+
+    linkInput.select()
+})
