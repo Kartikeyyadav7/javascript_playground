9fe47d3eb4187f849c0662aba11570e51bb3b90e
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-16 23:20:37 +0530

    done with the secret message

--- secret_msg/script.js
+++ secret_msg/script.js
@@ -1,11 +1,21 @@
+const { hash } = window.location
+
+const actualMessage = atob(hash.replace('#', ''))
+const message = document.querySelector("#message-input")
+const linkInput = document.querySelector("#link-input")
+
+if (actualMessage) {
+    document.querySelector("#message-secret").classList.remove('hide')
+    // message.classList.add('hide')
+    document.querySelector("#main").classList.add('hide')
+    document.querySelector('h3').innerHTML = actualMessage
+}
+
+
 document.querySelector("form").addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const message = document.querySelector("#message-input")
-
     const encrypted = btoa(message.value);
-
-    const linkInput = document.querySelector("#link-input")
 
     linkInput.value = `${window.location}#${encrypted}`
 
