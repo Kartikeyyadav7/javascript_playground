6ebf58e32d1de1f8b3332126e9a73410e066e1bf
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-20 23:48:33 +0530

    added some code to let us know if it is a file or folder

--- command_line_tool/index.js
+++ command_line_tool/index.js
@@ -1,8 +1,15 @@
 const fs = require('fs')
 
-fs.readdir(process.cwd(), (err, filesName) => {
+fs.readdir(process.cwd(), (err, filesNames) => {
     if (err) {
         console.log("Error occured", err)
     }
-    console.log(filesName)
+    for (let fileName of fileNames) {
+        fs.lstat(fileName, (err, stats) => {
+            if (err) {
+                console.log(err)
+            }
+            console.log(fileName, stats.isFile())
+        })
+    }
 })
