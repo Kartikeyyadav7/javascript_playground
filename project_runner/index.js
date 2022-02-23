0b8b1dbc7156a4508a8178e1f42891174e66acb8
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-23 21:46:53 +0530

    added caporal and file checking

--- project_runner/index.js
+++ project_runner/index.js
@@ -1,11 +1,30 @@
 const chokidar = require('chokidar')
 const debounce = require("lodash.debounce")
+const program = require('caporal')
+const fs = require('fs')
 
-const start = debounce(() => {
-    console.log("STARTING Users code")
-}, 100)
+program
+    .version('0.0.1')
+    .argument('[filename]', "Name of the file to run")
+    .action(async ({ filename }) => {
+        const name = filename || "index.js";
 
-chokidar.watch('.')
-    .on('add', start)
-    .on("change", () => console.log("FILE Changed"))
-    .on("unlink", () => console.log("FILE Deleted"))
+        try {
+            await fs.promises.access(name)
+        } catch (err) {
+            throw new Error(`Couldn't find the file ${name}`)
+        }
+
+
+        const start = debounce(() => {
+            console.log("STARTING Users code")
+        }, 100)
+
+        chokidar.watch('.')
+            .on('add', start)
+            .on("change", start)
+            .on("unlink", start)
+    })
+
+program.parse(process.argv)
+
