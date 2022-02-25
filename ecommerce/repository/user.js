55eb14aadf20c93a869ad2a5d532860a7974b9d0
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-25 21:53:46 +0530

    created a local database type system using repository model

---
+++ ecommerce/repository/user.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+class UserRepository {
+    constructor(fileName) {
+        if (!fileName) {
+            throw new Error('Need a filename to be passed while creating the user repo')
+        }
+
+        this.fileName = fileName;
+
+        try {
+            fs.accessSync(this.fileName)
+        }
+        catch (err) {
+            fs.writeFileSync(this.fileName, '[]')
+        }
+    }
+}
+
+new UserRepository('test.txt')
