2de197f6c41b3eed639d137b86287acb85cf826a
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-26 21:48:36 +0530

    finding user with particular ID

--- ecommerce/repository/user.js
+++ ecommerce/repository/user.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const crypto = require('crypto')
 
 class UserRepository {
     constructor(fileName) {
@@ -23,20 +24,35 @@ class UserRepository {
     }
 
     async create(attr) {
+        attr.id = this.randomId()
         const records = await this.getAll()
         records.push(attr);
+        this.writeAll(records)
+    }
+
+    async writeAll(records) {
         //* Here the '2' argument in JSON.stringify function is used to define the leve of indentation we need in the file itself so that it is easier to read
         await fs.promises.writeFile(this.fileName, JSON.stringify(records, null, 2))
+
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString('hex')
+    }
+
+    async getOne(id) {
+        const records = await this.getAll()
+
+        return records.find(record => record.id === id)
     }
 }
 
 const test = async () => {
 
     const repo = new UserRepository('user.json')
-    await repo.create({ email: "hello@gmail.com", password: "password" })
-    const users = await repo.getAll()
+    const user = await repo.getOne('65268097')
 
-    console.log(users)
+    console.log(user)
 }
 
 test()
