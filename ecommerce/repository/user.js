48a2939f77119da7ac1ec2d8800f6d54e8a65037
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-26 22:01:47 +0530

    added delete and update methods

--- ecommerce/repository/user.js
+++ ecommerce/repository/user.js
@@ -42,17 +42,36 @@ class UserRepository {
 
     async getOne(id) {
         const records = await this.getAll()
-
         return records.find(record => record.id === id)
+    }
+
+    async delete(id) {
+        const records = await this.getAll()
+        const filteredRecords = records.filter(record => record.id !== id)
+        await this.writeAll(filteredRecords)
+    }
+
+    async update(id, attr) {
+        const records = await this.getAll()
+        const record = records.find(record => record.id === id)
+
+        if (!record) {
+            throw new Error(`No record with the given ${id} ID`)
+        }
+        //* Object.assign copies the attr to the record object
+        Object.assign(record, attr)
+
+        await this.writeAll(records)
+
     }
 }
 
 const test = async () => {
 
     const repo = new UserRepository('user.json')
-    const user = await repo.getOne('65268097')
 
-    console.log(user)
+    await repo.update('671a689a', { confirmPassword1: "asdasdf" })
+
 }
 
 test()
