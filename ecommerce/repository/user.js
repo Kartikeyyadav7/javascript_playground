652bbab3dafc7c7b24b595b99c5bdab77b8ee32d
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-26 21:21:30 +0530

    added create new record functionality

--- ecommerce/repository/user.js
+++ ecommerce/repository/user.js
@@ -21,12 +21,19 @@ class UserRepository {
             encoding: 'utf8'
         }))
     }
+
+    async create(attr) {
+        const records = await this.getAll()
+        records.push(attr);
+        //* Here the '2' argument in JSON.stringify function is used to define the leve of indentation we need in the file itself so that it is easier to read
+        await fs.promises.writeFile(this.fileName, JSON.stringify(records, null, 2))
+    }
 }
 
 const test = async () => {
 
     const repo = new UserRepository('user.json')
-
+    await repo.create({ email: "hello@gmail.com", password: "password" })
     const users = await repo.getAll()
 
     console.log(users)
