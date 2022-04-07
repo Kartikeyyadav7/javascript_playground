f8e352933c5184066d7de709720a557059dda642
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-04-07 17:17:40 +0530

    added file upload

--- ecommerce/routes/admin/product.js
+++ ecommerce/routes/admin/product.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const multer = require('multer')
 const { requireTitle, requirePrice } = require('../../middlewares/validators');
 const ProductsRepository = require('../../repository/product')
 const router = express.Router()
 const { validationResult } = require("express-validator")
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 router.get("/admin/products", (req, res) => {
 
@@ -12,10 +15,21 @@ router.get('/admin/product/new', (req, res) => {
     res.render("newProduct")
 })
 
-router.post("/admin/product/new", [requireTitle, requirePrice], (req, res) => {
+router.post("/admin/product/new", upload.single('productImg'), [requireTitle, requirePrice], (req, res) => {
     const errors = validationResult(req)
-    console.log(errors)
-    res.send("submitted")
+
+    if (!errors.isEmpty()) {
+        return res.render("newProduct")
+    }
+    const { title, price } = req.body;
+    const image = req.file.buffer.toString('base64');
+
+
+
+    await ProductsRepository.create({ title, price, image });
+
+    res.send('submitted')
+
 })
 
 module.exports = router
